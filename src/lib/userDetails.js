57c9051c26d3c7e2483f2d3f6e7e2e9f5f760af0
sarwar <sarwar_github_email@example.com>
2026-07-26 20:40:05 +0600

    lib folder updated
    Signed-off-by: sarwar <sarwar_github_email@example.com>

--- src/lib/userDetails.js
+++
@@ -1,9 +0,0 @@
-import { headers } from "next/headers";
-import { auth } from "./auth";
-
-export  const UserDetails = async()=>{
-    const session = await  auth.api.getSession({
-        header:await headers()
-   })
-   return session;
-}
