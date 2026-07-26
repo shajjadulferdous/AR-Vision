b868b897ab35f9d874cd87595f319342ecf3e97a
sarwar <sarwar_github_email@example.com>
2026-07-26 21:03:50 +0600

    next config updated
    Signed-off-by: sarwar <sarwar_github_email@example.com>

--- next.config.mjs
+++ next.config.mjs
@@ -1,17 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  /* config options here */
   images: {
     remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: '*',
-        port: '',
-        pathname: '/**',
-        search: '',
-      },
+      { protocol: "https", hostname: "res.cloudinary.com" },
+      { protocol: "https", hostname: "images.unsplash.com" },
+      { protocol: "https", hostname: "plus.unsplash.com" },
+      { protocol: "https", hostname: "**" },
     ],
-  },  
+  },
 };
 
 export default nextConfig;
